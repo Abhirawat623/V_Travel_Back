6fece8677105a50841f5145086150a7e3c1b35ab
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-16 00:43:40 +0530

    hotel data added

--- routes/hotel.router.js
+++ routes/hotel.router.js
@@ -1,11 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const hotelRouter = express.Router();
 
-const hotels = require("../database/hotels")
-hotelRouter.route("/").
-get((req,res)=>{
-    res.json(hotels.data)
-})
+const Hotel = require("../routes/hotelDataImport.router");
 
-module.exports= hotelRouter;
+hotelRouter.route("/").get(async (req, res) => {
+  const hotelCategory = req.query.hotelCategory;
+  try {
+    let hotels;
+
+    if (hotelCategory) {
+      hotels = await Hotel.find({ cateory: hotelCategory });
+    } else {
+      hotels = await Hotel.find({});
+    }
+    hotels
+      ? res.json(hotels)
+      : res.status(404).json({ messae: "no data found" });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = hotelRouter;
