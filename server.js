1c6ca27288ecba9e4317bf242b16a2f4270c3e4c
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-18 00:41:40 +0530

    corse added

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 
 const PORT =3000;
 
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+app.use(cors());
+
 app.use(express.json());
 
 const connectDB = require('./config/dbconfig');
