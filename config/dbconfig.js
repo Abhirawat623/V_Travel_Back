758e8edbd608bf3c53872e905de1e8e58a51c17b
Abhishek Rawat <131130116+Abhirawat623@users.noreply.github.com>
2023-08-18 13:08:03 +0530

    errors handled 4

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
 const connectDB = async () => {
   try {
 
--- index.js
+++ index.js
@@ -1,61 +1,59 @@
-const express = require("express")
-const mongoose = require('mongoose');
-const app = express();
-
-const PORT =8080;
-
 const dotenv = require("dotenv");
-const cors = require("cors");
 
 dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const PORT = 8080;
+
+const cors = require("cors");
 
 app.use(cors());
 
 app.use(express.json());
 
-const connectDB = require('./config/dbconfig');
+const connectDB = require("./config/dbconfig");
 
 const hotelRouter = require("./routes/hotel.router");
 
-const categoriesRouter = require('./routes/categories.router');
+const categoriesRouter = require("./routes/categories.router");
 
-const hotelDbRouter= require('./routes/hotelDataImport.router');
+const hotelDbRouter = require("./routes/hotelDataImport.router");
 
 const hotelCategoryRouter = require("./routes/hotelCategoryDataImport.route");
 
-const singleHotelRouter= require("./routes/singleHotel.router");
+const singleHotelRouter = require("./routes/singleHotel.router");
 
-const {signUpRouter,logInRouter}= require('./routes/auth.router');
+const { signUpRouter, logInRouter } = require("./routes/auth.router");
 
 const wishlistRouter = require("./routes/wishlist.router");
 
 connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("hello users")
-})
+app.get("/", (req, res) => {
+  res.send("hello users");
+});
 
-app.use('/api/hotels',hotelRouter);
+app.use("/api/hotels", hotelRouter);
 
-app.use('/api/hoteldata',hotelDbRouter);
+app.use("/api/hoteldata", hotelDbRouter);
 
-app.use('/api/categorydata',hotelCategoryRouter);
+app.use("/api/categorydata", hotelCategoryRouter);
 
-app.use('/api/categories',categoriesRouter);
+app.use("/api/categories", categoriesRouter);
 
-app.use("/api/hotels",singleHotelRouter);
+app.use("/api/hotels", singleHotelRouter);
 
-app.use('/auth',signUpRouter);
+app.use("/auth", signUpRouter);
 
-app.use('/auth',logInRouter);
+app.use("/auth", logInRouter);
 
-app.use("/api/wishlist",wishlistRouter)
+app.use("/api/wishlist", wishlistRouter);
 
-mongoose.connection.once("open",()=>{
-    console.log('connected to DB');
-    app.listen(process.env.PORT || PORT,()=>{
-        console.log("server is running")
-    })
-    
-})
-
+mongoose.connection.once("open", () => {
+  console.log("connected to DB");
+  app.listen(process.env.PORT || PORT, () => {
+    console.log("server is running");
+  });
+});
